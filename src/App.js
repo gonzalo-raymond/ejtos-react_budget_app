ecb800ff90bd8e652c510f68f0347e5e2555165b
Gonzalo Raymond <gonchi_msn@hotmail.com>
2023-01-22 10:35:21 -0500

    Company's Budget Allocation Web-App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Add code to import the components
+import AllocationForm from "./components/AllocationForm";
+// eslint-disable-next-line
+import ExpenseItem from "./components/ExpenseItem";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseTotal from "./components/ExpenseTotal";
+import RemainingBudget from "./components/Remaining";
+import Budget from "./components/Budget";
 import { AppProvider } from './context/AppContext';
+
 const App = () => {
+
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
+
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+
+                <div className='row mt-3'>
+
+                    <div className='col-sm'>
+                        <Budget />
+                    </div> 
+
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+
+                <h3 className='mt-3'>Allocation</h3>
+
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+
+                <h3 className='mt-3'>Change allocation</h3>
+
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>   
+                </div>
             </div>
         </AppProvider>
     );
 };
+
 export default App;
