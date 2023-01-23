eb0a2ea90865a771f811905349aea81c014c744d
Gonzalo Raymond <gonchi_msn@hotmail.com>
2023-01-23 02:51:10 -0500

    Final Project Budget progress

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext} from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses} = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const editBudget = (newBudget) => {
+
+        if(newBudget > 20000){
+
+            alert("The value of the budget cannot exceed 20.000");
+
+        }else if(newBudget < totalExpenses){
+
+            alert("You cannot reduce the budget value lower than the spending");
+
+        }else{
+
+            dispatch({
+                type: "SET_BUDGET",
+                payload: parseInt(newBudget) 
+            });
+        }
+
+    }
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <label for="budget">Budget: £</label>
+            <input type="number" id="budget" value={budget} step="10" max="20000" onChange={(e) => editBudget(e.target.value)}/>
         </div>
     );
 };
