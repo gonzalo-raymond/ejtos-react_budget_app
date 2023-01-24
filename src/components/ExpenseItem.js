3a2af69df966d86ddfb4046968324b1d39993aef
Gonzalo Raymond <gonchi_msn@hotmail.com>
2023-01-24 02:22:32 -0500

    styling the increase/decrease btns

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import {FcPlus} from "react-icons/fc"
+import {FaMinusCircle} from "react-icons/fa"
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -45,9 +47,9 @@ const ExpenseItem = (props) => {
         <tr>
             <td>{props.name}</td>
             <td>{currency}{props.cost}</td>
-            <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-            <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
-            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td style={{textAlign: "center", paddingTop: "5px"}}><FcPlus size="3em" onClick={event=> increaseAllocation(props.name)}></FcPlus></td>
+            <td style={{textAlign: "center", paddingTop: "5px"}}><FaMinusCircle size="3em" style={{color: "#b01e11"}} onClick={event=> decreaseAllocation(props.name)}></FaMinusCircle></td>
+            <td style={{textAlign: "center", alignItems: "center"}}><TiDelete size='1.5em' style={{marginTop: "10px"}} onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
